cc1fd89df0f37923450753e784ac9aceb7ff79c2
Joan Vega <joan@inte.es>
2023-10-31 11:58:36 +0100

    Release Candidate v1.0.0

--- config/default.js
+++ config/default.js
@@ -13,7 +13,7 @@ module.exports = {
 
         'api': {
             host: '0.0.0.0',
-            port: 8090,
+            port: process.env.PORT || 8090,
 
             routes: {
                 'cors': {
